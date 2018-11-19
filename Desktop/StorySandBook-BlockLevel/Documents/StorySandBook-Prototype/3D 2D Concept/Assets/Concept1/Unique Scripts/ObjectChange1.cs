06929ba22ca0a455dad5d9c17bc97e88d4528305
Taylor Infuso <tinfuso@ucsc.edu>
2018-11-18 19:22:56 -0600

    Added a level I was working on earlier

---
+++ Desktop/StorySandBook-BlockLevel/Documents/StorySandBook-Prototype/3D 2D Concept/Assets/Concept1/Unique Scripts/ObjectChange1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectChange1 : MonoBehaviour {
+
+    // Use this for initialization
+    Vector3 originalPos;
+    Vector3 originalDim;
+    Vector3 originalScale;
+    private bool flat;
+    public float Layer2D = 0;
+
+    void Start () {
+        originalPos = this.gameObject.transform.position;
+        originalDim = this.gameObject.GetComponent<Renderer>().bounds.size;
+        originalScale = this.gameObject.transform.localScale;
+        flat = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("f") && flat == false)
+        {
+            Debug.Log("yeet");
+            this.gameObject.transform.Translate(0, 0, -originalPos.z - (Layer2D * 0.5f));
+            Vector3 scale = this.gameObject.transform.localScale;
+            scale.z = 0.1f;
+            this.gameObject.transform.localScale = scale;
+            flat = true;
+        } else if (Input.GetKeyDown("f") && flat == true)
+        {
+            Debug.Log("yote");
+            this.gameObject.transform.localScale = originalScale;
+            this.gameObject.transform.Translate(0, 0, originalPos.z + (Layer2D * 0.5f));
+            flat = false;
+        }
+	}
+}
---
+++ Desktop/StorySandBook-BlockLevel/Documents/StorySandBook-Prototype/3D 2D Concept/Assets/Concept1/Unique Scripts/PlayerMovement1.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement1 : MonoBehaviour {
+
+    GameObject character;
+    private bool flat;
+    private Vector3 originalPos;
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        flat = false;
+        character = this.gameObject;
+        rb = character.GetComponent<Rigidbody>();
+	}
+
+    //Set speed of player
+    public static float verSpeed = 6.0f;
+    public static float horSpeed = 6.0f;
+
+    // Update is called once per frame
+    void Update () {
+        if (flat == false)
+        {
+            float straffe = Input.GetAxis("Vertical") * verSpeed; //y input
+            float translation = Input.GetAxis("Horizontal") * horSpeed; //x input
+            translation *= Time.deltaTime; //calculate distance
+            straffe *= Time.deltaTime;
+            translation = Mathf.Clamp(translation, translation, translation);
+            character.transform.Translate(straffe, 0, -translation);
+            if (Input.GetKeyDown("f"))
+            {
+                originalPos = character.transform.position;
+                character.transform.Translate(0, 0, -originalPos.z);
+                flat = true;
+            }
+        } else if(flat == true)
+        {
+            float translation = Input.GetAxis("Horizontal") * verSpeed; //y input
+            translation *= Time.deltaTime; //calculate distance
+            translation = Mathf.Clamp(translation, translation, translation);
+            character.transform.Translate(translation, 0, 0);
+            if (Input.GetKeyDown("f"))
+            {
+                character.transform.Translate(0, 0, originalPos.z);
+                flat = false;
+            }
+        }
+        if (Input.GetKeyDown("space") && rb.velocity.y == 0)
+        {
+            rb.velocity = new Vector3(0, 10, 0);
+        }
+
+    }
+}
