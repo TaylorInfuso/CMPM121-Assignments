6ef0c3eeae65e4bf7a721a639b07f693dea3a50b
Taylor Infuso <tinfuso@ucsc.edu>
2018-12-21 15:54:46 -0600

    example of placeable doors

---
+++ Desktop/StorySandBook-PlacingDoorsConcept/StorySandBook-Prototype_2/3D 2D Concept/Assets/PlayTest_1/Unique Scripts/CheckPoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public Transform checkpoint;
+    private PlayerMovement2 pm;
+    GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        pm = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement2>();
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            player.transform.position = checkpoint.position;
+            print(checkpoint.position);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Body"))
+        {
+            pm.checkpos = transform.position;
+            print(pm.checkpos);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+  
+    
+}
---
+++ Desktop/StorySandBook-PlacingDoorsConcept/StorySandBook-Prototype_2/3D 2D Concept/Assets/PlayTest_1/Unique Scripts/TreeAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeAnimation : MonoBehaviour {
+    public Animator leafOut;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            print("hh");
+            gameObject.SetActive(false);
+            leafOut.SetTrigger("Leaf_Out");
+        }
+    }
+}
